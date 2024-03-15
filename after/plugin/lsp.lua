5a428a0b55634f0d9c81a9d0345238a59ae6509a
leyban <leybanlazada@gmail.com>
2024-03-15 15:32:41 +0800

    diabled pyright type checking to use ruff instead

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,6 +4,8 @@ lsp.on_attach(function(client, bufnr)
     -- see :help lsp-zero-keybindings
     -- to learn the available actions
     lsp.default_keymaps({ buffer = bufnr })
+    lsp.buffer_autoformat()
+
     local opts = { buffer = bufnr }
 
     vim.keymap.set('n', 'gd', '<cmd>Telescope lsp_definitions<cr>', opts)
@@ -11,25 +13,34 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', opts)
 
     vim.keymap.set('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>')
-
 end)
 
 -- (Optional) Configure lua language server for neovim
 -- require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
-lsp.format_on_save({
-    format_opts = {
-        async = false,
-        timeout_ms = 10000,
-    },
-    servers = {
-        ['lua_ls'] = { 'lua' },
-        ['gopls'] = { 'go' },
-        ['black'] = { 'python' },
-        -- if you have a working setup with null-ls
-        -- you can specify filetypes it can format.
-        -- ['null-ls'] = {'javascript', 'typescript'},
+-- lsp.format_on_save({
+--     format_opts = {
+--         async = false,
+--         timeout_ms = 10000,
+--     },
+--     servers = {
+--         ['lua_ls'] = { 'lua' },
+--         ['gopls'] = { 'go' },
+--         ['pyright'] = { 'python' },
+--     }
+-- })
+
+local lspconfig = require('lspconfig')
+lspconfig.pyright.setup{
+  on_attach = on_attach,
+  flags = lsp_flags,
+  settings = {
+    python = {
+      analysis = {
+        typeCheckingMode = "off"
+      }
     }
-})
+  }
+}
 
 lsp.setup()
