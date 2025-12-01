e0255d891e1cdc6f68273809f6dd0f1ab5e86b2a
Leyban <leybanlazada@gmail.com>
2025-12-01 18:23:06 +0800

    Customize buf.hover window

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -32,6 +32,7 @@ require('mason-lspconfig').setup({
     },
 })
 
+
 lspconfig('pyright', {
     settings = {
         python = {
@@ -49,20 +50,41 @@ lspconfig('emmet_ls', {
     -- on_attach = on_attach,
     capabilities = capabilities,
     init_options = {
-      html = {
-        options = {
-          -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-          ["bem.enabled"] = true,
+        html = {
+            options = {
+                -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+                ["bem.enabled"] = true,
+            },
         },
-      },
     }
 })
 vim.lsp.enable('emmet_ls', {
-    "css", "templ", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue" 
+    "css", "templ", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact",
+    "vue"
 })
 
 vim.lsp.enable('tailwindcss', {
     "templ", "html", "jsx", "tsx"
 })
+
+local severity = vim.diagnostic.severity
+
+vim.diagnostic.config({
+    signs = {
+        text = {
+            [severity.ERROR] = " ",
+            [severity.WARN] = " ",
+            [severity.HINT] = "󰠠 ",
+            [severity.INFO] = " ",
+        },
+    },
+})
+
+local orig = vim.lsp.buf.hover
+vim.lsp.buf.hover = function()
+    return orig({
+        border = "solid",
+    })
+end
 
 lsp.setup()
